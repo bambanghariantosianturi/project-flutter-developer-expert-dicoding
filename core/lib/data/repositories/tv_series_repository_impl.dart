e0d8953c29bc2798ea13c932055e36bef9f48aac
bams26 <bambang.sianturi@alodokter.com>
2023-11-23 11:41:14 +0700

    add const server failure popular tv series

--- core/lib/data/repositories/tv_series_repository_impl.dart
+++ core/lib/data/repositories/tv_series_repository_impl.dart
@@ -36,7 +36,7 @@ class TvSeriesRepositoryImpl extends TvSeriesRepository {
       final result = await remoteDataSource.getPopularTvSeries();
       return Right(result.map((model) => model.toEntity()).toList());
     } on ServerException {
-      return Left(ServerFailure(''));
+      return const Left(ServerFailure(''));
     } on SocketException {
       return Left(ConnectionFailure('Failed to connect to the network'));
     }
