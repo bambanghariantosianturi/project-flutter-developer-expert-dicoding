09452ba0a491f7a75c526ab8dd8b7e2aba93c1d1
bams26 <bambang.sianturi@alodokter.com>
2023-12-13 09:50:24 +0700

    add const box decoration

--- tvseries/lib/presentation/page/tv_series_detail_page.dart
+++ tvseries/lib/presentation/page/tv_series_detail_page.dart
@@ -90,7 +90,7 @@ class DetailContent extends StatelessWidget {
           child: DraggableScrollableSheet(
             builder: (context, scrollController) {
               return Container(
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: kRichBlack,
                   borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
                 ),
